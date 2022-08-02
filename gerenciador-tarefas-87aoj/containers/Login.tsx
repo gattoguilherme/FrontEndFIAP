3fe94bede3259ed0907d444e626804e51e0e72b1
Logon Aluno <logonrmlocal@fiap.com.br>
2022-08-01 22:54:42 -0300

    login formatado

--- gerenciador-tarefas-87aoj/containers/Login.tsx
+++ gerenciador-tarefas-87aoj/containers/Login.tsx
@@ -1,7 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
+import Image from 'next/image'
 
 export const Login = () => {
     return (
-        <div>Teste</div>
+        <div className="container-login">
+            <img src="/logo.svg" alt="Logo Fiap" className="logo"/>
+            <form>
+                <div className="input">
+                    <img src="/mail.svg" alt="Logo Fiap"/>
+                    <input type="text" placeholder="Login"></input>
+                </div>
+                <div className="input">
+                    <img src="/lock.svg" alt="Informe sua senha"/>
+                    <input type="password" placeholder="Senha"></input>
+                </div>
+                <div>
+                    <button> Login </button>
+                </div>
+            </form>
+        </div>
     );
 }
